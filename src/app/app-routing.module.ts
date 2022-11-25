a0f40f13b30a3ef60c8a8f8252c1af4512539ea3
Pannatat Saman <pannatat@idpassglobal.com>
2022-11-25 13:49:56 +0700

    START

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerFormComponent } from './container/customer-form/customer-form.component';
 import { DashboardComponent } from './container/dashboard/dashboard.component';
@@ -10,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule,RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
